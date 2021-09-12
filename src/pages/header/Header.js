3c8e681438997b089d13cd49d61cef4041f0aec1
Ajea Smith <smithajea@rocketmail.com>
2021-09-12 10:27:22 -0400

    added logo and menu links to navbar

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import logo from '../../logo.png';
 const Header = () => {
-	return <div>im the header</div>;
+	return (
+		<header>
+			<img src={logo} alt="graphic of lady reading book" />
+			<nav>
+				<ul>
+					<li>Profile</li>
+					<li>Login</li>
+					<li>Register</li>
+					<li>Logout</li>
+					<li>Tell Story</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;

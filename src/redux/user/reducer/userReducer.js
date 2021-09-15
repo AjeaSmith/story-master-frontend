548b6c60bf23d7e3a33a8ce3c6ad0318587c41ef
Ajea Smith <smithajea@rocketmail.com>
2021-09-14 21:00:57 -0400

    created user action - register

--- src/redux/user/reducer/userReducer.js
+++ src/redux/user/reducer/userReducer.js
@@ -1,10 +1,16 @@
 const initalState = {
 	user: null,
+	message: null,
+	error: null,
 };
 const userReducer = (state = initalState, action) => {
 	switch (action.type) {
 		case 'REGISTER':
 			return { ...state, user: action.payload };
+		case 'REGISTER_SUCCESS':
+			return { ...state, message: action.payload };
+		case 'REGISTER_ERROR':
+			return { ...state, error: action.payload };
 		case 'LOGIN':
 			return { ...state, user: action.payload };
 		default:

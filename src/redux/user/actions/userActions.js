7b30e75e402b44e7e4c4c4aa17ac1f0c649b3a44
Ajea Smith <smithajea@rocketmail.com>
2021-09-17 19:29:12 -0400

    switch to production url

--- src/redux/user/actions/userActions.js
+++ src/redux/user/actions/userActions.js
@@ -4,13 +4,12 @@ export const Register = ({ username, email, password }) => async (dispatch) => {
 	dispatch({ type: 'REGISTER_PENDING' });
 
 	await axios
-		.post('http://localhost:8080/api/user/register', {
+		.post('https://story-master-backend.herokuapp.com/api/user/register', {
 			username,
 			email,
 			password,
 		})
 		.then(({ data }) => {
-			console.log(data);
 			dispatch({ type: 'REGISTER_SUCCESS', payload: data.msg });
 		})
 		.catch(({ response }) => {

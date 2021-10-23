3774602d2d3c54c8728415a9634341716766cc44
Ajea Smith <smithajea@rocketmail.com>
2021-10-23 01:17:19 -0400

    worked on edit profile page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import EditProfile from './pages/profile/EditProfile';
 import { createBrowserHistory } from 'history';
 import axios from 'axios';
-import Header from './components/header/Header';
 
 const customHistory = createBrowserHistory();
 
@@ -17,7 +18,6 @@ function App() {
 	`;
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	useEffect(() => {
-		console.log('render app');
 		return axios
 			.get('http://localhost:8080/api/user/authenticated', {
 				withCredentials: true,
@@ -42,8 +42,12 @@ function App() {
 					<Route path="/register" component={Register} />
 					<Route path="/login" component={Login} />
 					<Route
-						path="/profile"
+						path="/profile/:id"
 						render={() => <Profile isLoggedIn={isLoggedIn} />}
+					/>
+					<Route
+						path="/edit/profile/:id"
+						render={() => <EditProfile isLoggedIn={isLoggedIn} />}
 					/>
 				</Switch>
 			</Router>

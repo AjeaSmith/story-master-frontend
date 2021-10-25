911a8cc7ffd9e1f4b6c842c232a9e501ef5dc55a
Ajea Smith <smithajea@rocketmail.com>
2021-10-25 15:18:12 -0400

    finished home page desing

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,22 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Banner from '../../components/banner/Banner';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import {
-	StyledSection,
-	StyledContent,
-	StyledContainer,
-	StyledStory,
-	StyledUser,
-	StyledDetails,
-	StyledImg,
-	StyledAuthor,
-	StyledTime,
-	StyledName,
-	StyledHeading2,
-	StyledPara,
-} from './HomeStyle';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import { StyledSection, StyledContent, StyledContainer } from './HomeStyle';
+
 const Home = () => {
+	const authState = useSelector((state) => state.authState);
 	return (
 		<>
 			<Banner>
@@ -24,7 +21,7 @@ const Home = () => {
 					style={{
 						maxWidth: '600px',
 						margin: '0 auto',
-						padding: '3em 0',
+						padding: '6em 0.5em',
 						display: 'flex',
 						justifyContent: 'center',
 						alignItems: 'center',
@@ -40,6 +37,7 @@ const Home = () => {
 						Share your story around the world!
 					</Typography>
 					<Typography
+						align="center"
 						variant="subtitle1"
 						sx={{ fontWeight: '500', margin: '12px 0' }}
 					>
@@ -47,76 +45,107 @@ const Home = () => {
 						Expedita saepe eos error harum quos praesentium odio
 						voluptatum quas reprehenderit. Explicabo!
 					</Typography>
-					<Button
-						variant="contained"
-						sx={{ backgroundColor: '#18A999', margin: '12px 0' }}
-					>
-						Ready to share your story?
-					</Button>
+					{authState.authenticated ? (
+						<Button
+							variant="contained"
+							sx={{ backgroundColor: '#18A999', margin: '12px 0' }}
+						>
+							<Link to="/story/create" style={{ color: 'white' }}>
+								Ready to share your story?
+							</Link>
+						</Button>
+					) : (
+						<Button
+							variant="contained"
+							sx={{ backgroundColor: '#18A999', margin: '12px 0' }}
+						>
+							<Link to="/login" style={{ color: 'white' }}>
+								Log in to share your story!
+							</Link>
+						</Button>
+					)}
 				</div>
 			</Banner>
 			<StyledSection>
 				<StyledContainer>
 					<StyledContent>
-						<h2 style={{ textAlign: 'center' }}>
+						<h2 style={{ textAlign: 'center', margin: '0.3em 0' }}>
 							Awesome stories made by you!
 						</h2>
-						<hr />
-						<StyledStory>
-							<StyledUser>
-								<StyledName>
-									<StyledImg
-										src="https://via.placeholder.com/50"
-										alt="placeholder"
+						<Divider />
+						<List
+							sx={{
+								width: '100%',
+								bgcolor: 'background.paper',
+								margin: '0 auto',
+							}}
+						>
+							<ListItem alignItems="flex-start">
+								<ListItemAvatar>
+									<Avatar
+										sx={{ width: 40, height: 40 }}
+										alt="Remy Sharp"
+										src="https://picsum.photos/50/50?grayscale"
 									/>
-									<StyledAuthor>
-										James -{' '}
-										<span style={{ fontSize: '14px', color: 'gray' }}>
-											Michigan
-										</span>
-									</StyledAuthor>
-								</StyledName>
-								<StyledTime>
-									<small>5m ago</small>
-								</StyledTime>
-							</StyledUser>
-							<StyledDetails>
-								<StyledHeading2>My great story</StyledHeading2>
-								<StyledPara>
-									Lorem ipsum dolor sit, amet consectetur adipisicing
-									elit. Minima sed ducimus facere!
-								</StyledPara>
-								<hr />
-							</StyledDetails>
-						</StyledStory>
-
-						<StyledStory>
-							<StyledUser>
-								<StyledName>
-									<StyledImg
-										src="https://via.placeholder.com/50"
-										alt="placeholder"
+								</ListItemAvatar>
+								<ListItemText
+									sx={{ fontSize: '12px' }}
+									primary={`@James Blake - Oct 26`}
+									secondary={
+										<React.Fragment>
+											<h3>sit amet consectetur</h3>
+											<Typography
+												sx={{ display: 'inline' }}
+												component="span"
+												variant="body2"
+												color="text.primary"
+											>
+												Lorem ipsum dolor sit amet consectetur
+												adipisicing elit. Pariatur dolores tempore
+												aliquid perspiciatis sapiente sequi aperiam
+												ex provident neque repellendus!
+											</Typography>
+										</React.Fragment>
+									}
+								/>
+								<Button>View</Button>
+							</ListItem>
+							<Divider variant="inset" component="li" />
+							<ListItem alignItems="flex-start">
+								<ListItemAvatar>
+									<Avatar
+										sx={{ width: 40, height: 40 }}
+										alt="Remy Sharp"
+										src="https://picsum.photos/50/50?grayscale"
 									/>
-									<StyledAuthor>
-										James -{' '}
-										<span style={{ fontSize: '14px', color: 'gray' }}>
-											Michigan
-										</span>
-									</StyledAuthor>
-								</StyledName>
-								<StyledTime>
-									<small>5m ago</small>
-								</StyledTime>
-							</StyledUser>
-							<StyledDetails>
-								<StyledHeading2>My great story</StyledHeading2>
-								<StyledPara>
-									Lorem ipsum dolor sit, amet consectetur adipisicing
-									elit. Minima sed ducimus facere!
-								</StyledPara>
-								<hr />
-							</StyledDetails>
-						</StyledStory>
+								</ListItemAvatar>
+								<ListItemText
+									sx={{ fontSize: '12px' }}
+									primary={`@Admin - Oct 28`}
+									secondary={
+										<React.Fragment>
+											<h3>The long project home</h3>
+											<Typography
+												sx={{ display: 'inline' }}
+												component="span"
+												variant="body2"
+												color="text.primary"
+											>
+												Lorem ipsum dolor sit amet consectetur
+												adipisicing elit. Pariatur dolores tempore
+												aliquid perspiciatis sapiente sequi aperiam
+												ex provident neque repellendus!
+											</Typography>
+										</React.Fragment>
+									}
+								/>
+								<Button>
+									<Link to="/view/story" style={{ color: '#18A999' }}>
+										View
+									</Link>
+								</Button>
+							</ListItem>
+						</List>
 					</StyledContent>
 				</StyledContainer>
 			</StyledSection>
--- src/pages/home/HomeStyle.js
+++ src/pages/home/HomeStyle.js
@@ -1,19 +1,4 @@
 import styled from 'styled-components';
-
-export const StyledHeading = styled.h1`
-	font-size: 2rem;
-	margin-bottom: 0;
-`;
-export const StyledHeading2 = styled.h2`
-	margin-top: 0;
-	margin-bottom: 10px;
-	font-size: 20px;
-`;
-export const StyledSubheading = styled.p`
-	font-size: 16px;
-	font-weight: normal;
-	line-height: 1.4;
-`;
 export const StyledSection = styled.section`
 	background-color: #ffffff;
 	height: 90vh;
@@ -31,35 +16,4 @@ export const StyledContent = styled.div`
 	border: 1px solid #e4e4e7;
 	background-color: #fff;
 	border-radius: 5px;
-`;
-export const StyledStory = styled.div`
-	padding: 0 12px;
-`;
-export const StyledUser = styled.div`
-	padding: 1em 1em;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-`;
-export const StyledImg = styled.img`
-	border-radius: 50px;
-`;
-export const StyledDetails = styled.div`
-	padding: 0 1em;
-`;
-export const StyledAuthor = styled.small`
-	font-size: 18px;
-	margin-left: 8px;
-`;
-export const StyledTime = styled.div`
-	color: #6d28d9;
-`;
-export const StyledName = styled.div`
-	display: flex;
-	align-items: center;
-`;
-export const StyledPara = styled.p`
-	margin-top: 0;
-	margin-bottom: 2em;
-	color: #71717a;
 `;
